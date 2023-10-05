462160d0833727f089a791b83947f04fa136f1e7
Umut Yenidil <umut.yenidil9@gmail.com>
2023-10-05 14:21:12 +0300

    nodejs course 5.9

--- data/product_data.js
+++ data/product_data.js
@@ -1,34 +1,34 @@
 const productList = [
-    {
-        name: 'Urun 1',
-        description: 'Birinci urunun aciklamasi',
-        image:'https://picsum.photos/512/512?random=1',
-        price: '110',
-    },
-    {
-        name: 'Urun 2',
-        description: 'Ikinci urunun aciklamasi',
-        image:'https://picsum.photos/512/512?random=2',
-        price: '220',
-    },
-    {
-        name: 'Urun 3',
-        description: 'Ucuncu urunun aciklamasi',
-        image:'https://picsum.photos/512/512?random=3',
-        price: '330',
-    },
-    {
-        name: 'Urun 4',
-        description: 'Dorduncu urunun aciklamasi',
-        image:'https://picsum.photos/512/512?random=4',
-        price: '440',
-    },
-    {
-        name: 'Urun 5',
-        description: 'Besinci urunun aciklamasi',
-        image:'https://picsum.photos/512/512?random=5',
-        price: '550',
-    },
+    // {
+    //     name: 'Urun 1',
+    //     description: 'Birinci urunun aciklamasi',
+    //     image:'https://picsum.photos/512/512?random=1',
+    //     price: '110',
+    // },
+    // {
+    //     name: 'Urun 2',
+    //     description: 'Ikinci urunun aciklamasi',
+    //     image:'https://picsum.photos/512/512?random=2',
+    //     price: '220',
+    // },
+    // {
+    //     name: 'Urun 3',
+    //     description: 'Ucuncu urunun aciklamasi',
+    //     image:'https://picsum.photos/512/512?random=3',
+    //     price: '330',
+    // },
+    // {
+    //     name: 'Urun 4',
+    //     description: 'Dorduncu urunun aciklamasi',
+    //     image:'https://picsum.photos/512/512?random=4',
+    //     price: '440',
+    // },
+    // {
+    //     name: 'Urun 5',
+    //     description: 'Besinci urunun aciklamasi',
+    //     image:'https://picsum.photos/512/512?random=5',
+    //     price: '550',
+    // },
 ];
 
 module.exports.productList = productList;
