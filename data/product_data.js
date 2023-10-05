26cd9c486e9aac1decbab0a30cddcb0bb127b348
Umut Yenidil <umut.yenidil9@gmail.com>
2023-10-05 16:26:43 +0300

    nodejs course 6.7

--- data/product_data.js
+++ data/product_data.js
@@ -1,29 +1,34 @@
 const productList = [
     {
+        uuid: '7005ae44-13a0-45f6-a389-51738fef3c1a',
         name: 'Urun 1',
         description: 'Birinci urunun aciklamasi',
         image:'https://picsum.photos/512/512?random=1',
         price: '110',
     },
     {
+        uuid: '6f4540e9-9d4f-4a6f-a859-b30a8735182a',
         name: 'Urun 2',
         description: 'Ikinci urunun aciklamasi',
         image:'https://picsum.photos/512/512?random=2',
         price: '220',
     },
     {
+        uuid: '7f8a0200-1fea-4bdf-948c-add381ef5348',
         name: 'Urun 3',
         description: 'Ucuncu urunun aciklamasi',
         image:'https://picsum.photos/512/512?random=3',
         price: '330',
     },
     {
+        uuid: 'c31fef99-c32b-4bc7-a66b-e51f13992062',
         name: 'Urun 4',
         description: 'Dorduncu urunun aciklamasi',
         image:'https://picsum.photos/512/512?random=4',
         price: '440',
     },
     {
+        uuid: '4750b206-de9d-4fd4-b678-4356883e7ed1',
         name: 'Urun 5',
         description: 'Besinci urunun aciklamasi',
         image:'https://picsum.photos/512/512?random=5',
--- models/product.js
+++ models/product.js
@@ -1,7 +1,10 @@
+const { v4: uuidv4 } = require('uuid');
+
 const productData = require('../data/product_data');
 
 module.exports = class Product{
     constructor ({name, description, price, image}){
+        this.uuid = uuidv4();
         this.name = name;
         this.description = description;
         this.price = price;
